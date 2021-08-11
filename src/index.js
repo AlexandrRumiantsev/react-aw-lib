d6644f7243094ef5f160d41a19b30bd478002086
alex <tte577es7@yandex.ru>
2021-08-11 22:04:07 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// Demo component
+// this is only example component
+// you can find tests in __test__ folder
+
+import React from 'react';
+
+class MyComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className='s-alert-box-inner'>
+                    jhjghj
+                </div>
+                <span className='s-alert-close'></span>
+            </div>
+        )
+    }
+}
+
+export default MyComponent;
